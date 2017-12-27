81d86a57c38f0c14dbfd808e76dd97f8ea85d6a3
stnch <34873836+stnch@users.noreply.github.com>
2017-12-27 04:39:06 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web-puppet.z192.lan':}
   dockeragent::node {'db-puppet.z192.lan':}
+  host { 'web-puppet.z192.lan':
+    ensure => present,
+    ip     => '172.18.0.2',
+  }
+  host { 'db-puppet.z192.lan':
+    ensure => present,
+    ip     => '172.18.0.3',
+  }
 }
